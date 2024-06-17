50727b092304715633edaca29cac1b4c519f5624
Miteos1986 <mdmitriy1986@gmail.com>
2024-06-17 14:14:56 +0200

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,48 @@ import ImageGallery from "./components/ImageGallery/ImageGallery"
 
 
 function App() {
-
+// image render
   const [images, setImages] = useState([])
+  // loading render
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     const getImages = async () => {
-      const data = await getArticlesApi()
-      console.log('data :>> ', data);
-      setImages(data)
+
+      try {
+        setIsLoading(true)
+  
+        const data = await getArticlesApi()
+        console.log('data :>> ', data);
+        setImages(data)
+  
+                
+      } catch (error) {
+        setError(true)
+      } finally {
+        setIsLoading(false)
+      }
+
     }
     getImages()
   },[])
 
  
-return <>{images.length > 0&& (
+return <>
+{error && (
+  <p>
+    <b>Oooops...some error</b>
+  </p>
+) }
+
+{images.length > 0&& (
 <ImageGallery images={images} />
 )
 }
+{isLoading && (<p>
+  <b>loading...</b>
+</p>)}
 </>
 
 
